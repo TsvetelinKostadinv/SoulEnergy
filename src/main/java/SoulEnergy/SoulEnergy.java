6d1f39f5992029dce1ef1a50c4ceab3b4105bc68
Tsvetelin Kostadinov <tsvetelinkostadinovts@gmail.com>
2018-06-11 14:20:54 +0300

    The base of the mod A empty starting class mcmod.info And a base package

---
+++ src/main/java/SoulEnergy/SoulEnergy.java
@@ -0,0 +1,12 @@
+package SoulEnergy;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(useMetadata = true,
+        modid = "soulen",
+        name = "Soul Energy",
+        version = "1.0")
+public class SoulEnergy
+{
+
+}
